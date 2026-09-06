f77b5b38034f39a7f1302e595b6a1d0a73386cfd
Mark Vu <markvu2607@miava.io.vn>
2026-09-06 14:59:20 +0700

    fix: fix neovim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -11,61 +11,55 @@ vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 -- Install lazy.nvim if not already installed
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com",
-    "--branch=stable",
-    lazypath,
-  })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 vim.opt.rtp:prepend(lazypath)
 
 -- Install plugins using lazy.nvim
-local status_ok, lazy = pcall(require, "lazy")
-if status_ok then
-  lazy.setup({
-    -- [mini.surround]: Thao tác bao quanh ký tự (ngoặc đơn, ngoặc kép)
-    {
-      "echasnovski/mini.surround",
-      version = "*",
-      opts = {},
-    },
+require("lazy").setup({
+	-- [mini.surround]: Thao tác bao quanh ký tự (ngoặc đơn, ngoặc kép)
+	{
+		"echasnovski/mini.surround",
+		version = "*",
+		opts = {},
+	},
 
-    -- [mini.ai]: Nâng cấp Text Objects cực mạnh (Chọn hàm, chọn tham số)
-    {
-      "echasnovski/mini.ai",
-      version = "*",
-      opts = { n_lines = 500 },
-    },
+	-- [mini.ai]: Nâng cấp Text Objects cực mạnh (Chọn hàm, chọn tham số)
+	{
+		"echasnovski/mini.ai",
+		version = "*",
+		opts = { n_lines = 500 },
+	},
 
-    -- [mini.comment]: Comment code nhanh chóng
-    { 'echasnovski/mini.comment', version = '*', opts = {} },
+	-- [mini.comment]: Comment code nhanh chóng
+	{ 'echasnovski/mini.comment', version = '*', opts = {} },
 
-    -- [targets.vim]: Mở rộng các đối tượng văn bản (text objects) trong Vim
-    { 'wellle/targets.vim' },
-
-    -- [flash.nvim]: Nhảy con trỏ tới bất kỳ vị trí nào trên màn hình
-    {
-      "folke/flash.nvim",
-      event = "VeryLazy",
-      opts = {},
-      keys = {
-        { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
-        { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
-      },
-    },
-  }, {
-    -- Vô hiệu hóa giao diện cài đặt vì chạy ẩn trong VS Code
-    ui = { enabled = false },
-    defaults = { lazy = false },
-    performance = {
-      rtp = {
-        disabled_plugins = {
-          "gzip", "tarPlugin", "tohtml", "tutor", "zipPlugin", "netrwPlugin",
-        },
-      },
-    },
-  })
-end
+	-- [flash.nvim]: Nhảy con trỏ tới bất kỳ vị trí nào trên màn hình
+	{
+		"folke/flash.nvim",
+		event = "VeryLazy",
+		opts = {},
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash Jump" },
+			{ "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+		},
+	},
+}, {
+	-- Vô hiệu hóa giao diện cài đặt vì chạy ẩn trong VS Code
+	ui = { enabled = false },
+	defaults = { lazy = false },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip", "tarPlugin", "tohtml", "tutor", "zipPlugin", "netrwPlugin",
+			},
+		},
+	},
+})
